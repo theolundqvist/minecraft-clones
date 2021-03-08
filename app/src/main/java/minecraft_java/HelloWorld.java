e1a6323f223abfcfb59b7f85a593a173571fd606
Lundqvist <theodor.lundqvist@gmail.com>
2021-03-08 20:48:34 +0100

    asd

--- app/src/main/java/minecraft_java/HelloWorld.java
+++ app/src/main/java/minecraft_java/HelloWorld.java
@@ -1,5 +1,7 @@
 package minecraft_java;
 
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
 import org.lwjgl.*;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
@@ -9,7 +11,7 @@ import java.nio.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL46.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
@@ -17,6 +19,10 @@ public class HelloWorld {
 
 	// The window handle
 	private long window;
+	private static float zoom = 20;
+	private static int mouseX, mouseY;
+	private static final Vector3f center = new Vector3f();
+	private static float pitch = 0.3f, yaw = 0.2f;
 
 	public void run() {
 		System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -75,6 +81,14 @@ public class HelloWorld {
 				(vidmode.width() - pWidth.get(0)) / 2,
 				(vidmode.height() - pHeight.get(0)) / 2
 			);
+			glfwSetCursorPosCallback(window, (win, x, y) -> {
+				if (glfwGetMouseButton(win, GLFW_MOUSE_BUTTON_1) == GLFW_PRESS) {
+					yaw += ((int) x - mouseX) * 0.01f;
+					pitch += ((int) y - mouseY) * 0.01f;
+				}
+				mouseX = (int) x;
+				mouseY = (int) y;
+			});
 		} // the stack frame is popped automatically
 
 		// Make the OpenGL context current
@@ -86,6 +100,7 @@ public class HelloWorld {
 		glfwShowWindow(window);
 	}
 
+
 	private void loop() {
 		// This line is critical for LWJGL's interoperation with GLFW's
 		// OpenGL context, or any context that is managed externally.
@@ -94,13 +109,28 @@ public class HelloWorld {
 		// bindings available for use.
 		GL.createCapabilities();
 
+		this.setupQuad();
+
+		Matrix4f cam = new Matrix4f();
+		FloatBuffer fb = BufferUtils.createFloatBuffer(16);
+
 		// Set the clear color
 		glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-
 		// Run the rendering loop until the user has attempted to close
 		// the window or has pressed the ESCAPE key.
 		while ( !glfwWindowShouldClose(window) ) {
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+			glLoadMatrixf(
+				cam.translation(0, 0, 0)
+                   .rotateX(pitch)
+                   .rotateY(yaw)
+                   //.translate(-center.x, -center.y, -center.z)
+                   .get(fb)
+			);
+
+			//DRAW
+			this.loopCycle();
+			
 
 			glfwSwapBuffers(window); // swap the color buffers
 
@@ -108,6 +138,81 @@ public class HelloWorld {
 			// invoked during this call.
 			glfwPollEvents();
 		}
+	}
+
+	// Quad variables
+	private int vaoId = 0;
+	private int vboId = 0;
+	private int vboiId = 0;
+	private int indicesCount = 0;
+	public void setupQuad() {
+		// Vertices, the order is not important.
+		float[] vertices = { -0.5f, 0.5f, 0f, // Left top ID: 0
+				-0.5f, -0.5f, 0f, // Left bottom ID: 1
+				0.5f, -0.5f, 0f, // Right bottom ID: 2
+				0.5f, 0.5f, 0f // Right left ID: 3
+		};
+		// Sending data to OpenGL requires the usage of (flipped) byte buffers
+		FloatBuffer verticesBuffer = BufferUtils.createFloatBuffer(vertices.length);
+		verticesBuffer.put(vertices);
+		verticesBuffer.flip();
+
+		// OpenGL expects to draw vertices in counter clockwise order by default
+		byte[] indices = {
+				// Left bottom triangle
+				0, 1, 2,
+				// Right top triangle
+				2, 3, 0 };
+		indicesCount = indices.length;
+		ByteBuffer indicesBuffer = BufferUtils.createByteBuffer(indicesCount);
+		indicesBuffer.put(indices);
+		indicesBuffer.flip();
+
+		// Create a new Vertex Array Object in memory and select it (bind)
+		// A VAO can have up to 16 attributes (VBO's) assigned to it by default
+		vaoId = glGenVertexArrays();
+		glBindVertexArray(vaoId);
+
+		// Create a new Vertex Buffer Object in memory and select it (bind)
+		// A VBO is a collection of Vectors which in this case resemble the location of
+		// each vertex.
+		vboId = glGenBuffers();
+		glBindBuffer(GL_ARRAY_BUFFER, vboId);
+		glBufferData(GL_ARRAY_BUFFER, verticesBuffer, GL_STATIC_DRAW);
+		// Put the VBO in the attributes list at index 0
+		glVertexAttribPointer(0, 3, GL_FLOAT, false, 0, 0);
+		// Deselect (bind to 0) the VBO
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		// Deselect (bind to 0) the VAO
+		glBindVertexArray(0);
+
+		// Create a new VBO for the indices and select it (bind)
+		vboiId = glGenBuffers();
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboiId);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indicesBuffer, GL_STATIC_DRAW);
+		// Deselect (bind to 0) the VBO
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+	}
+
+	public void loopCycle() {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Bind to the VAO that has all the information about the vertices
+		glBindVertexArray(vaoId);
+		glEnableVertexAttribArray(0);
+
+		// Bind to the index VBO that has all the information about the order of the
+		// vertices
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vboiId);
+
+		// Draw the vertices
+		glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_BYTE, 0);
+
+		// Put everything back to default (deselect)
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+		glDisableVertexAttribArray(0);
+		glBindVertexArray(0);
 	}
 
 	public static void main(String[] args) {
