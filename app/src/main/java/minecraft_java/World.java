69cec468f5d4fa1fa0b024550a56ab0779c29e76
Ludvig Delvret <ludvig.delvret.lu@gmail.com>
2021-03-09 20:04:06 +0100

    load and unload function

--- app/src/main/java/minecraft_java/World.java
+++ app/src/main/java/minecraft_java/World.java
@@ -7,7 +7,7 @@ public class World {
     private HashMap<Key, Chunk> loadedChunks;
     private HashMap<Key, Chunk> unloadedChunks;
     private final int chunkSize;
-    private int loadChunks = 1;
+    private int renderDistance = 1;
 
     public World(int chunkSize) {
         this.chunkSize = chunkSize;
@@ -22,7 +22,7 @@ public class World {
         Key playerChunk = getPlayerChunk(p);
         if(!oldPlayerChunk.equals(playerChunk)){
             oldPlayerChunk = playerChunk;
-            loadNewChunks();
+            loadNewChunks(p);
         }
     }
 
@@ -30,11 +30,22 @@ public class World {
         return new Key((int) p.getPos().x/chunkSize, (int) p.getPos().z/chunkSize);
     }
 
-    private void loadNewChunks(){
-
-
+    private void loadNewChunks(Player p){
+        Key k = getPlayerChunk(p);
+        HashMap<Key, Chunk> temp = new HashMap<Key, Chunk>(loadedChunks);
         //om new Key inte existerar put(new chunk)
-
+        for (int i = -renderDistance; i <= renderDistance; i++) {
+            for (int j = -renderDistance; j <= renderDistance; j++) {
+                k = new Key(k.x += i, k.z += j);
+                if (!(loadedChunks.containsKey(k))){
+                    loadedChunks.put(k, getChunk(k));
+                } else {
+                    temp.remove(k);
+                }
+            }
+            unloadedChunks.putAll(temp);
+            temp.keySet().forEach(key -> loadedChunks.remove(key));
+        }
     }
     
     private static class Key{
