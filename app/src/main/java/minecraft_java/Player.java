06ccddb96d35ce49fdcfb63bfe9e32c59d963d90
Ludvig Delvret <ludvig.delvret.lu@gmail.com>
2021-03-09 18:08:53 +0100

    Intial commit of Player with constructors

---
+++ app/src/main/java/minecraft_java/Player.java
@@ -0,0 +1,23 @@
+package minecraft_java;
+
+public class Player {
+    private float x;
+    private float y;
+    private float z;
+
+
+    public Player() {
+        this.x = 0f;
+        this.y = 1f;
+        this.z = 0f;
+    }
+
+
+    public Player(float x, float y, float z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+
+}
